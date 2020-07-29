878f400e65a37ce25d18bd47e414f99c7d69d290
Valentin Krivobokov <krivobokovvv@yandex.ru>
2020-07-29 12:05:47 +0300

    Add application "comics".

---
+++ comics/admin.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import Book, Chapter, Page, Tag
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    raw_id_fields = ('tags',)
+    search_fields = ('name',)
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'name', 'count_page')
+    list_filter = ('book',)
+    search_fields = ('name',)
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chapter', 'number', 'image')
+    list_filter = ('chapter',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+    search_fields = ('name',)
---
+++ comics/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from sorl.thumbnail import ImageField
+from colorfield.fields import ColorField
+
+
+class Book(models.Model):
+	class Meta:
+		verbose_name = _('Book')
+		verbose_name_plural = _('Books')
+
+	name = models.CharField(verbose_name=_('Title'), max_length=200)
+	author = models.CharField(verbose_name=_('Author'), max_length=200, blank=True, null=True)
+	tags = models.ManyToManyField('Tag', blank=True)
+	preview = models.ForeignKey('Page', verbose_name=_('Preview'), on_delete=models.SET_NULL, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+
+class Chapter(models.Model):
+	class Meta:
+		verbose_name = _('Chapter')
+		verbose_name_plural = _('Chapters')
+
+	book = models.ForeignKey('Book', verbose_name='Книга', on_delete=models.CASCADE)
+	name = models.CharField(verbose_name=_('Title'), max_length=50)
+	count_page = models.IntegerField(verbose_name='Количество страниц', default=0)
+
+	def __str__(self):
+		return self.name
+
+
+class Page(models.Model):
+	class Meta:
+		verbose_name = _('Page')
+		verbose_name_plural = _('Pages')
+
+		unique_together = ('chapter', 'number')
+
+	chapter = models.ForeignKey('Chapter', verbose_name=_('Chapter'), on_delete=models.CASCADE)
+	number = models.IntegerField(verbose_name=_('Number'))
+	image = ImageField(verbose_name=_('Image'), upload_to='image/%Y/%m/%d/')
+
+	def __str__(self):
+		page_test = _('Page')
+		return f'{self.chapter.name} / {page_test}. {self.number}'
+
+
+class Tag(models.Model):
+	class Meta:
+		verbose_name = _('Tag')
+		verbose_name_plural = _('Tags')
+
+	name = models.CharField(verbose_name=_('Name'), max_length=50)
+	color = ColorField(verbose_name=_('Color'), default='#FFFFFF')

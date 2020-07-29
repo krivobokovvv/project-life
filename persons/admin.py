75a2c49284acd69c81d355f952b4b97c579e18f2
Valentin Krivobokov <krivobokovvv@yandex.ru>
2020-07-29 12:07:33 +0300

    Add application "persons".

---
+++ persons/admin.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import Person, Contact, ContactType, Company
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'birthday',
+        'company',
+        'position',
+        'deleted',
+    )
+    list_filter = ('birthday', 'company', 'deleted')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'person', 'contact_type')
+    list_filter = ('person', 'contact_type')
+
+
+@admin.register(ContactType)
+class ContactTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'regex')
+    search_fields = ('name',)
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
---
+++ persons/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class Person(models.Model):
+	class Meta:
+		verbose_name = _('Person')
+		verbose_name_plural = _('Persons')
+
+	first_name = models.CharField(verbose_name=_('First name'), max_length=100)
+	middle_name = models.CharField(verbose_name=_('Middle name'), max_length=100, blank=True, null=True)
+	last_name = models.CharField(verbose_name=_('Last name'), max_length=100, blank=True, null=True)
+	birthday = models.DateField(verbose_name=_('Birthday'), blank=True, null=True)
+	company = models.ForeignKey('Company', on_delete=models.PROTECT, blank=True, null=True)
+	position = models.CharField(verbose_name=_('Position'), max_length=100, blank=True, null=True)
+	nick = models.CharField(verbose_name=_('Nick'), max_length=30, blank=True, null=True)
+
+	deleted = models.BooleanField(verbose_name=_('Deleted?'), default=False)
+
+
+class Contact(models.Model):
+	class Meta:
+		verbose_name = _('Contact')
+		verbose_name_plural = _('Contacts')
+
+	person = models.ForeignKey('Person', on_delete=models.PROTECT)
+	contact_type = models.ForeignKey('ContactType',on_delete=models.PROTECT)
+	value = models.CharField(verbose_name=_('Value'), max_length=100)
+
+
+class ContactType(models.Model):
+	class Meta:
+		verbose_name = _('Contact type')
+		verbose_name_plural = _('Contact types')
+	
+	name = models.CharField(verbose_name=_('Name'), max_length=100)
+	regex = models.CharField(verbose_name=_('Regular expression'), max_length=100, blank=True, null=True)
+
+
+class Company(models.Model):
+	class Meta:
+		verbose_name = _('Company')
+		verbose_name = _('Companies')
+
+	name = models.CharField(verbose_name=_('Name'), max_length=100)
+	address = models.CharField(verbose_name=_('Address'), max_length=150, blank=True, null=True)

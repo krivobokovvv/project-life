470e5fb84ffbb6dcee77856d8d529a65c46edcad
Valentin Krivobokov <krivobokovvv@yandex.ru>
2020-07-31 12:00:46 +0300

    Add routes to tasks and main urls'.

--- life/urls.py
+++ life/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
-urlpattern = [
+urlpatterns = [
+	path('tasks/', include('tasks.urls')),
 	path('admin/', admin.site.urls),
 	path('i18n/', include('django.conf.urls.i18n')),
+	path('', include('main.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

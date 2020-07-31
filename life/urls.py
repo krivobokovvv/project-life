770800c0d1ecb3fe873792c12f70c07581bb26d0
Valentin Krivobokov <krivobokovvv@yandex.ru>
2020-07-31 11:59:30 +0300

    Delete i18n_patterns from urls.

--- life/urls.py
+++ life/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from django.conf.urls.i18n import i18n_patterns
 
 urlpattern = [
 	path('admin/', admin.site.urls),
 	path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-urlpatterns = i18n_patterns(
-	path('', include(urlpattern)),
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

40ef4b1b919c95cfd62e2e659594bd9a84f91e4d
Valentin Krivobokov <krivobokovvv@yandex.ru>
2020-11-25 17:08:37 +0300

    Update urls.

--- life/urls.py
+++ life/urls.py
@@ -17,21 +17,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('tasks/', include('tasks.urls')),
+    url('i18n/', include('django.conf.urls.i18n')),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('', include('main.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-    import debug_toolbar
-
     urlpatterns = [
-                      path('__debug__/', include(debug_toolbar.urls)),
+                    path('__debug__/', include(debug_toolbar.urls)),
                   ] + urlpatterns
